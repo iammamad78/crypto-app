476ba8eb3afc5063235ba66a267c3300df9ef15f
Mamad78 <mamadi78.94@gmail.com>
2025-09-21 02:05:28 +0330

    folder structure & testing api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import HomePage from "./components/templates/HomePage";
+
 function App() {
   return (
     <>
-      <h3>crypto app</h3>
+      <HomePage />
     </>
   );
 }
---
+++ src/components/modules/TableCoin.jsx
@@ -0,0 +1,7 @@
+function TableCoin({ coins }) {
+  console.log(coins);
+
+  return <div>TableCoin</div>;
+}
+
+export default TableCoin;
