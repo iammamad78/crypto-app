2de85b787b2eb072bbb5aae7a4f2862b7d76fafa
Mamad78 <mamadi78.94@gmail.com>
2025-09-23 00:56:13 +0330

    refact TableRow added

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -2,12 +2,9 @@ import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
 function TableCoin({ coins }) {
-  console.log(coins);
-
   return (
     <div>
       <table>
-        
         <thead>
           <tr>
             <th>Coin</th>
@@ -15,32 +12,13 @@ function TableCoin({ coins }) {
             <th>Price</th>
             <th>24H</th>
             <th>Total Volume</th>
+            <th>Chart</th>
           </tr>
         </thead>
 
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>${coin.current_price.toLocaleString()}</td>
-              <td>{coin.market_cap_change_percentage_24h.toFixed(2)}</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td>
-                <img
-                  src={
-                    coin.market_cap_change_percentage_24h > 0
-                      ? chartUp
-                      : chartDown
-                  }
-                />
-              </td>
-            </tr>
+            <TableRow coin={coin} key={coin.id} />
           ))}
         </tbody>
       </table>
@@ -49,3 +27,32 @@ function TableCoin({ coins }) {
 }
 
 export default TableCoin;
+
+const TableRow = ({
+  coin: {
+    image,
+    symbol,
+    name,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} />
+      </td>
+    </tr>
+  );
+};
