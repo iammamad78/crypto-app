9dcc1317685808a921f38f0d5577c2ce0299f49b
Mamad78 <mamadi78.94@gmail.com>
2025-09-24 01:57:47 +0330

    Styles added to Pagination

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,5 +1,6 @@
+import styles from "./Pagination.module.css";
+
 function Pagination({ page, setPage }) {
-    
   const previousHandler = () => {
     if (page <= 1) return;
     setPage((page) => page - 1);
@@ -11,22 +12,32 @@ function Pagination({ page, setPage }) {
   };
 
   return (
-    <div>
-      <button onClick={previousHandler}>previous</button>
-      <p style={{ color: page === 1 ? "red" : "inherit" }}>1</p>
-      <p style={{ color: page === 2 ? "red" : "inherit" }}>2</p>
+    <div className={styles.pagination}>
+      <button
+        className={page === 1 ? styles.disabled : null}
+        onClick={previousHandler}
+      >
+        previous
+      </button>
+      <p className={page === 1 && styles.selected}>1</p>
+      <p className={page === 2 && styles.selected}>2</p>
 
       {page > 2 && page < 9 && (
         <>
           <span>...</span>
-          <p>{page}</p>
+          <p className={styles.selected}>{page}</p>
         </>
       )}
 
       <span>...</span>
-      <p>9</p>
-      <p>10</p>
-      <button onClick={nextHandler}>next</button>
+      <p className={page === 9 && styles.selected}>9</p>
+      <p className={page === 10 && styles.selected}>10</p>
+      <button
+        className={page === 10 ? styles.disabled : null}
+        onClick={nextHandler}
+      >
+        next
+      </button>
     </div>
   );
 }
