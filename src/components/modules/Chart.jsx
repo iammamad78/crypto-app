f6f67730b0e0be4f816095a3cb3eacfb7fff88cd
Mamad78 <mamadi78.94@gmail.com>
2025-10-03 16:48:38 +0330

    Rechart Library added

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import { convertData } from "../../helpers/convertData";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
@@ -12,9 +21,26 @@ function Chart({ chart, setChart }) {
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+function ChartComponent({ data, type }) {
+  return (
+    <LineChart width={760} height={300} data={data}>
+      <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth="2px" />
+      <CartesianGrid stroke="#404042" />
+      <YAxis dataKey={type} domain={["auto", "auto"]} />
+      <XAxis dataKey="date" hide />
+      <Legend />
+      <Tooltip />
+    </LineChart>
+  );
+}
